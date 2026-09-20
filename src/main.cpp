855bf9dc1d69444ec7e7030c5ca9d169ce613209
Bastien <baba.andre26@gmail.com>
2026-09-20 23:26:10 +0200

    CommonLibNg Port

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,122 @@
-void OnDataLoaded()
-{
-	SKSE::log::info("Data loaded");
+class LevelUpMenuCloser final : public RE::BSTEventSink<RE::MenuOpenCloseEvent> {
+public:
+	static LevelUpMenuCloser* GetSingleton() {
+		static LevelUpMenuCloser singleton;
+		return &singleton;
+	}
+
+	RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* a_event,
+		RE::BSTEventSource<RE::MenuOpenCloseEvent>*) override {
+
+		if (!a_event || !a_event->opening) {
+			return RE::BSEventNotifyControl::kContinue;
+		}
+
+		// Level up menu opening
+		if (a_event->menuName == RE::LevelUpMenu::MENU_NAME) {
+			if (auto* ui = RE::UI::GetSingleton()) {
+				if (auto menu = ui->GetMenu(RE::LevelUpMenu::MENU_NAME)) {
+					if (menu->uiMovie) {
+						// Hide UI if not Skyrim VR
+						if (!REL::Module::IsVR()) {
+							menu->uiMovie->SetVisible(false);
+						}
+
+						SKSE::GetTaskInterface()->AddTask([]() {
+							auto* ui = RE::UI::GetSingleton();
+							if (!ui || !ui->IsMenuOpen(RE::LevelUpMenu::MENU_NAME)) {
+								return;
+							}
+
+							auto menu = ui->GetMenu(RE::LevelUpMenu::MENU_NAME);
+							if (!menu || !menu->uiMovie) {
+								return;
+							}
+
+							// Pick magicka and accept
+							RE::GFxValue emptyArgs;
+							menu->uiMovie->CreateArray(&emptyArgs);
+							RE::GFxValue args[2];
+							args[0].SetString("addMagicka");
+							args[1] = emptyArgs;
+							RE::GFxValue result;
+							menu->uiMovie->Invoke("_global.gfx.io.GameDelegate.call", &result, args, 2);
+						});
+					}
+				}
+			}
+		}
+		// Confirmation box opening right after
+		else if (a_event->menuName == RE::MessageBoxMenu::MENU_NAME) {
+			if (auto* ui = RE::UI::GetSingleton(); ui && ui->IsMenuOpen(RE::LevelUpMenu::MENU_NAME)) {
+				if (auto menu = ui->GetMenu(RE::MessageBoxMenu::MENU_NAME)) {
+					if (menu->uiMovie) {
+						// Hide UI if not Skyrim VR
+						if (!REL::Module::IsVR()) {
+							menu->uiMovie->SetVisible(false);
+						}
+
+						SKSE::GetTaskInterface()->AddTask([]() {
+							auto* ui = RE::UI::GetSingleton();
+							if (!ui || !ui->IsMenuOpen(RE::MessageBoxMenu::MENU_NAME)) {
+								return;
+							}
+
+							auto menu = ui->GetMenu(RE::MessageBoxMenu::MENU_NAME);
+							if (!menu || !menu->uiMovie) {
+								return;
+							}
+
+							// Get Current Magicka (before)
+							float magickaBefore = 0.0f;
+							auto* player = RE::PlayerCharacter::GetSingleton();
+							RE::ActorValueOwner* avOwner = player ? player->AsActorValueOwner() : nullptr;
+							if (avOwner) {
+								magickaBefore = avOwner->GetBaseActorValue(RE::ActorValue::kMagicka);
+							}
+
+							// Accept
+							RE::GFxValue indexArray;
+							menu->uiMovie->CreateArray(&indexArray);
+							RE::GFxValue zero(0.0);
+							indexArray.SetElement(0, zero);
+							RE::GFxValue args[2];
+							args[0].SetString("buttonPress");
+							args[1] = indexArray;
+							RE::GFxValue result;
+							menu->uiMovie->Invoke("_global.gfx.io.GameDelegate.call", &result, args, 2);
+
+							// Restore Magicka
+							if (avOwner) {
+								avOwner->SetBaseActorValue(RE::ActorValue::kMagicka, magickaBefore);
+							}
+						});
+					}
+				}
+			}
+		}
+
+		return RE::BSEventNotifyControl::kContinue;
+	}
+
+private:
+	LevelUpMenuCloser() = default;
+};
+
+void OnDataLoaded() {
+	auto* ui = RE::UI::GetSingleton();
+	if (!ui) {
+		SKSE::log::error("NoLevelUpMenu: RE::UI singleton not available at kDataLoaded, cannot register sink.");
+		return;
+	}
+
+	ui->GetEventSource<RE::MenuOpenCloseEvent>()->AddEventSink(LevelUpMenuCloser::GetSingleton());
 }
 
-void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
-{
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg) {
 	switch (a_msg->type) {
 	case SKSE::MessagingInterface::kDataLoaded:
+		SKSE::log::info("NoLevelUpMenu: kDataLoaded received, installing hook.");
 		OnDataLoaded();
 		break;
 	case SKSE::MessagingInterface::kPostLoad:
@@ -14,20 +124,18 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 	case SKSE::MessagingInterface::kPreLoadGame:
 		break;
 	case SKSE::MessagingInterface::kPostLoadGame:
-        break;
+		break;
 	case SKSE::MessagingInterface::kNewGame:
 		break;
 	}
 }
 
-SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
-{
-	SKSE::Init(a_skse);
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+	SKSE::Init(skse);
 
-	SKSE::log::info("Start");
-
-    auto messaging = SKSE::GetMessagingInterface();
+	auto messaging = SKSE::GetMessagingInterface();
 	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+		SKSE::log::error("NoLevelUpMenu: failed to register SKSE messaging listener.");
 		return false;
 	}
 
